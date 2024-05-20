9ca59eed211f13c768c64daac01c2c85257a6fe5
petrushya <108490184+petrushya@users.noreply.github.com>
2024-05-20 21:44:25 +0300

    creates screen version game.js

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
 
+const board = document.querySelector('.board');
+const player1 = document.querySelector('.player1 .player');
+const player2 = document.querySelector('.player2 .player');
+
 const gameBoard = (function() {
   const board = [];
   while(board.length < 9) board.push(Cell());
@@ -26,15 +30,18 @@ const makeCounter = function() {
 
 const roundCounter = makeCounter();
 
-function gameController(playerOneName = 'player one', playerTwoName = 'player two') {
+const players = [
+  { name: 'player one', mark: '🗙' },
+  { name: 'player two', mark: '🞇' }
+];
+
+player1.textContent = players[0].name;
+player2.textContent = players[1].name;
+
+function gameController(playerOneName, playerTwoName) {
 
   const getBoard = gameBoard.getBoard();
   console.log(getBoard.map(cell => cell.getMark()));
-
-  const players = [
-    { name: playerOneName, mark: '🗙' },
-    { name: playerTwoName, mark: '🞇' }
-  ];
 
   const activePlayer = () => players[roundCounter.getValue() % 2];
 
@@ -54,3 +61,31 @@ function gameController(playerOneName = 'player one', playerTwoName = 'player tw
 
   return { playRound, getBoard };
 }
+
+function screenController() {
+  const game = gameController();
+  const boardDiv = document.querySelector('.board');
+  boardDiv.style.backgroundColor = '#000';
+  const board = game.getBoard;
+
+  while(boardDiv.firstChild) boardDiv.removeChild(boardDiv.firstChild);
+
+  board.forEach((cell, index) => {
+    const cellButton = document.createElement("div");
+    cellButton.classList.add("cell");
+    cellButton.dataset.cell = index;
+    boardDiv.appendChild(cellButton);
+  });
+
+  function clickHandlerBoard(e) {
+    const selectedCell = e.target.dataset.cell;
+    if(!selectedCell) return;
+    game.playRound(+selectedCell);
+    document.querySelector(`.cell[data-cell="${+selectedCell}"]`).textContent = board[+selectedCell].getMark();
+  }
+
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+}
+
+screenController();
