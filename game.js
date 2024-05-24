15f6cea57ef720954657e2254aa06ae848b604f5
petrushya <108490184+petrushya@users.noreply.github.com>
2024-05-24 23:16:35 +0300

    changes the game design, adds a dialog for entering player names, changes the player creation function

--- game.js
+++ game.js
@@ -1,46 +1,78 @@
+const playerOne = document.querySelector('.player1 .player');
+const playerTwo = document.querySelector('.player2 .player');
+const playerOneNamed = document.querySelector('#playerOneNamed');
+const playerTwoNamed = document.querySelector('#playerTwoNamed');
+const scoreOnePlayer = document.querySelector('.player1 .score');
+const scoreTwoPlayer = document.querySelector('.player2 .score');
+const maxCount = document.querySelector('#gameCounter');
+const output = document.querySelector('.gameCounterOutput');
+const round = document.querySelector('.round');
+const nextRound = document.querySelector('.nextRound');
+const gameInfo = document.querySelector('.gameInfo');
+const changeName = document.querySelector('.changeName');
+const namedDialog = document.querySelector('#namedDialog');
+const confirmNames = document.querySelector('#confirmNames');
+const playGame = document.querySelector('.play');
+const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
 
 const roundCounter = makeCounter();
 const playerOneCounter = makeCounter();
 const playerTwoCounter = makeCounter();
 const gameCounter = makeCounter();
-const playerOne = createPlayer(playerOneName = 'player one', '🗙', playerOneCounter);
-const playerTwo = createPlayer(playerTwoName = 'player two', '🞇', playerTwoCounter);
-const player1 = document.querySelector('.player1 .player');
-const player2 = document.querySelector('.player2 .player');
-const scoreOnePlayer = document.querySelector('.player1 .score');
-const scoreTwoPlayer = document.querySelector('.player2 .score');
-player1.textContent = playerOne.getPlayerName().toUpperCase();
-player2.textContent = playerTwo.getPlayerName().toUpperCase();
+const gameBoard = createBoard('.board').getBoard();
+let players = getPlayers(playerOne.textContent, playerTwo.textContent).players;
 
-const maxCount = document.querySelector('#gameCounter');
-const output = document.querySelector('.gameCounterOutput');
+changeName.addEventListener("click", () => {
+  namedDialog.showModal();
+});
+confirmNames.addEventListener("click", (e) => {
+  e.preventDefault();
+  if(playerOneNamed.value) playerOne.textContent = playerOneNamed.value.toUpperCase();
+  if(playerTwoNamed.value) playerTwo.textContent = playerTwoNamed.value.toUpperCase();
+  players = getPlayers(playerOne.textContent, playerTwo.textContent).players;
+  playerOneNamed.value = "";
+  playerTwoNamed.value = "";
+  namedDialog.close();
+  playGame.focus();
+});
+namedDialog.addEventListener("close", () => {
+  playerOneNamed.value = "";
+  playerTwoNamed.value = "";
+  changeName.blur();
+});
+
 output.textContent = maxCount.value;
 maxCount.addEventListener('input', () => {output.textContent = maxCount.value;});
-const round = document.querySelector('.round');
-const nextRound = document.querySelector('.nextRound');
-const playGame = document.querySelector('.play');
-const gameInfo = document.querySelector('.gameInfo');
-nextRound.addEventListener('click', () => {
-  roundCounter.resetCounter();
-  nextRound.style.visibility = 'hidden';
-  gameController();
-});
+
 playGame.addEventListener('click', () => {
   gameInfo.style.display = 'none';
   round.style.visibility = 'visible';
   [roundCounter, playerOneCounter, playerTwoCounter, gameCounter].forEach(element => element.resetCounter());
-  gameCounter.getValue() + 1 === +maxCount.value ? round.textContent = 'Last Round' :
-    round.textContent = `Round ${gameCounter.getValue() + 1}`;
+  round.textContent = `Round ${gameCounter.getValue() + 1} of ${maxCount.value}`;
   gameController();
 });
-
-const gameBoard = createBoard('.board').getBoard();
+nextRound.addEventListener('click', () => {
+  roundCounter.resetCounter();
+  gameCounter.getValue() + 1 === +maxCount.value ? round.textContent = 'Last Round' :
+    round.textContent = `Round ${gameCounter.getValue() + 1} of ${maxCount.value}`;
+  nextRound.style.visibility = 'hidden';
+  gameController();
+});
 
 function gameController() {
   const message = document.querySelector('.message');
   const boardDiv = document.querySelector('.board');
   const winArr = [];
-  const players = [playerOne, playerTwo];
+
   const activePlayer = () => players[roundCounter.getValue() % 2];
 
   scoreOnePlayer.textContent = playerOneCounter.getValue();
@@ -51,72 +83,59 @@ function gameController() {
     cell.classList.remove("color");
   });
 
-  const winCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2],
-  ];
-
-  function clickHandlerBoard(e) {
+  function clickHandlerBoard(event){
     while(winArr.firstChild) winArr.removeChild(winArr.firstChild);
-    const selectedCell = e.target.dataset.cell;
+    const selectedCell = event.target.dataset.cell;
     if(!selectedCell) return;
     const cell = document.querySelector(`.cell[data-cell="${+selectedCell}"]`);
     if(cell.textContent === ''){
-      cell.textContent = activePlayer().getPlayerMark();
+      cell.textContent = activePlayer().mark;
       winCombinations.forEach(elem => {
-        if(document.querySelector(`.cell[data-cell="${elem[0]}"]`).textContent === activePlayer().getPlayerMark() &&
-        document.querySelector(`.cell[data-cell="${elem[1]}"]`).textContent === activePlayer().getPlayerMark() &&
-        document.querySelector(`.cell[data-cell="${elem[2]}"]`).textContent === activePlayer().getPlayerMark()){
+        if(document.querySelector(`.cell[data-cell="${elem[0]}"]`).textContent === activePlayer().mark &&
+        document.querySelector(`.cell[data-cell="${elem[1]}"]`).textContent === activePlayer().mark &&
+        document.querySelector(`.cell[data-cell="${elem[2]}"]`).textContent === activePlayer().mark){
           winArr.push(elem[0], elem[1], elem[2]);
         };
       });
       if(winArr.length > 0){
         winArr.forEach(cell => document.querySelector(`.cell[data-cell="${cell}"]`).classList.add("color"));
-        message.innerHTML = `${activePlayer().getPlayerName().toUpperCase()}<br>won the round!<br>Score`;
+        message.textContent = `SCORE`;
         gameCounter.increment();
-        activePlayer().getPlayerCounter().increment();
+        activePlayer().counter.increment();
         scoreOnePlayer.textContent = playerOneCounter.getValue();
         scoreTwoPlayer.textContent = playerTwoCounter.getValue();
+        round.textContent = `${activePlayer().name} won the round!`;
         nextRound.style.visibility = 'visible';
-        gameCounter.getValue() + 1 === +maxCount.value ? round.textContent = 'Last Round' :
-          round.textContent = `Round ${gameCounter.getValue() + 1}`;
         boardDiv.removeEventListener("click", clickHandlerBoard);
       }else if(roundCounter.getValue() === 8){
         boardDiv.removeEventListener("click", clickHandlerBoard);
         gameCounter.increment();
-        message.innerHTML = 'Round Drow<br>score';
+        message.textContent = 'SCORE';
         nextRound.style.visibility = 'visible';
-        gameCounter.getValue() + 1 === +maxCount.value ? round.textContent = 'Last Round' :
-          round.textContent = `Round ${gameCounter.getValue() + 1}`;
+        round.textContent = `Round Drow`;
       }else{
         roundCounter.increment();
-        message.innerHTML = `Next mark<br><span class="mark">${activePlayer().getPlayerMark()}</span>`;
+        message.innerHTML = `Next mark<br><span class="mark">${activePlayer().mark}</span>`;
       };
       if(gameCounter.getValue() === +maxCount.value){
         gameInfo.style.display = 'flex';
         const para = gameInfo.querySelector('p');
         (playerOneCounter.getValue() > playerTwoCounter.getValue()) ?
-          para.innerHTML = `GAME OVER,<br>WINNER<br>${playerOne.getPlayerName().toUpperCase()}`:
-          para.innerHTML = `GAME OVER,<br>WINNER<br>${playerTwo.getPlayerName().toUpperCase()}`;
+          para.innerHTML = `GAME OVER,<br>WINNER<br>${players[0].name}`:
+          para.innerHTML = `GAME OVER,<br>WINNER<br>${players[1].name}`;
         if(playerOneCounter.getValue() === playerTwoCounter.getValue())
           para.innerHTML = 'GAME OVER<br>IN A DROW';
-        winArr.length > 0 ? message.innerHTML = `${activePlayer().getPlayerName().toUpperCase()}<br>won the round!<br>FINAL SCORE`:
-          message.innerHTML = `Round Drow<br>FINAL SCORE`;
-        round.style.visibility = 'hidden';
+        winArr.length > 0 ? round.textContent = `${activePlayer().name} won the round!`:
+          round.textContent = `Round Drow`;
+        message.textContent = 'FINAL SCORE';
         nextRound.style.visibility = 'hidden';
       };
     };
   };
   boardDiv.addEventListener("click", clickHandlerBoard);
-}
+};
 
-function createBoard(divNode) {
+function createBoard(divNode){
   const board = [];
   while(board.length < 9) board.push('');
   const boardDiv = document.querySelector(divNode);
@@ -127,24 +146,22 @@ function createBoard(divNode) {
     boardDiv.appendChild(cellDiv);
   });
   return { getBoard };
-}
+};
 
-function makeCounter() {
+function makeCounter(){
   let count = 0;
-  function changeValue(value) {count += value};
+  function changeBy(value) {count += value};
   return{
-    increment () {changeValue(1)},
-    resetCounter () {changeValue(-count)},
+    increment () {changeBy(1)},
+    resetCounter () {changeBy(-count)},
     getValue () {return count}
   };
 };
 
-function createPlayer(playerName, playerMark, playerCounter) {
-  const player = {name: playerName, mark: playerMark, counter: playerCounter};
-  return{
-    getPlayerName () {return player.name},
-    getPlayerMark () {return player.mark},
-    getPlayerCounter () {return player.counter}
-  };
+function getPlayers(playerOneName, playerTwoName){
+  const players = [
+    {name: playerOneName, mark: '🗙', counter: playerOneCounter},
+    {name: playerTwoName, mark: '🞇', counter: playerTwoCounter}
+  ];
+  return { players };
 };
-
