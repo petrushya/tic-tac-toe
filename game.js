401349a7b23c4dea2276ffd6afffa2c9ae2d5a80
petrushya <108490184+petrushya@users.noreply.github.com>
2024-06-14 18:43:45 +0300

    fixes some game.js code fragments

--- game.js
+++ game.js
@@ -1,12 +1,6 @@
 
 const playerOneName = document.querySelector('.player1 .player');
 const playerTwoName = document.querySelector('.player2 .player');
-const playerOneScore = document.querySelector('.player1 .score');
-const playerTwoScore = document.querySelector('.player2 .score');
-const message = document.querySelector('.message');
-const nextRound = document.querySelector('.nextRound');
-const round = document.querySelector('.round');
-const gameInfo = document.querySelector('.gameInfo');
 const playerOneNamed = document.querySelector('#playerOneNamed');
 const playerTwoNamed = document.querySelector('#playerTwoNamed');
 const maxCount = document.querySelector('#gameCounter');
@@ -15,12 +9,9 @@ const namedDialog = document.querySelector('#namedDialog');
 const confirmNames = document.querySelector('#confirmNames');
 const playGame = document.querySelector('.play');
 const changeName = document.querySelector('.changeName');
-const boardDiv = document.querySelector('.board');
-
-const playerOneCounter = makeCounter();
-const playerTwoCounter = makeCounter();
-const runCounter = makeCounter();
-const gameCounter = makeCounter();
+const round = document.querySelector('.round');
+const gameInfo = document.querySelector('.gameInfo');
+const nextRound = document.querySelector('.nextRound');
 
 changeName.addEventListener("click", () => {
   namedDialog.showModal();
@@ -43,24 +34,43 @@ namedDialog.addEventListener("close", () => {
 output.textContent = maxCount.value;
 maxCount.addEventListener('input', () => {output.textContent = maxCount.value;});
 
+const gamePlayers = (function(){
+  const players = [
+    {name:playerOneName.textContent,mark:'🗙',counter:makeCounter()},
+    {name:playerTwoName.textContent,mark:'🞇',counter:makeCounter()}
+  ];
+  const stepCounter = makeCounter();
+  const activePlayer = () => players[stepCounter.getValue() % 2];
+  return{
+    activePlayer,
+    runCounter(){return stepCounter},
+    getPlayers(){return players}
+  };
+})();
+
+const gameCounter = makeCounter();
+
 playGame.addEventListener('click', () => {
   gameInfo.style.display = 'none';
   round.style.visibility = 'visible';
-  [runCounter, playerOneCounter, playerTwoCounter, gameCounter].forEach(item => item.reset());
+  [gamePlayers.runCounter(), gamePlayers.getPlayers()[0].counter,
+  gamePlayers.getPlayers()[1].counter, gameCounter].forEach(item => item.reset());
   gamePlay();
-  gameCounter.getValue() + 1 === +maxCount.value ? round.textContent = 'Last Round' :
-    round.textContent = `Round ${gameCounter.getValue() + 1} of ${maxCount.value}`;
 });
 nextRound.addEventListener('click', () => {
-  runCounter.reset();
+  gamePlayers.runCounter().reset();
   nextRound.style.visibility = 'hidden';
   gamePlay();
-  gameCounter.getValue() + 1 === +maxCount.value ? round.textContent = 'Last Round' :
-    round.textContent = `Round ${gameCounter.getValue() + 1} of ${maxCount.value}`;
 });
 
 function gamePlay(){
+  const playerOneScore = document.querySelector('.player1 .score');
+  const playerTwoScore = document.querySelector('.player2 .score');
+  const message = document.querySelector('.message');
+  const boardDiv = document.querySelector('.board');
+
   const game = playController();
+  const activePlayer = () => gamePlayers.activePlayer();
   while(boardDiv.firstChild) boardDiv.removeChild(boardDiv.firstChild);
   const board = game.board;
   board.forEach((row, indexY) => {
@@ -76,11 +86,13 @@ function gamePlay(){
     })
   });
 
-  playerOneScore.textContent = playerOneCounter.getValue();
-  playerTwoScore.textContent = playerTwoCounter.getValue();
+  gameCounter.getValue() + 1 === +maxCount.value ? round.textContent = 'Last Round' :
+    round.textContent = `Round ${gameCounter.getValue() + 1} of ${maxCount.value}`;
+  playerOneScore.textContent = gamePlayers.getPlayers()[0].counter.getValue();
+  playerTwoScore.textContent = gamePlayers.getPlayers()[1].counter.getValue();
   message.innerHTML = 'First mark<br><span class="mark">&#x1F5D9;</span>';
-  const activePlayer = () => game.activePlayer();
 
+  boardDiv.addEventListener("click", clickHandlerBoard);
   function clickHandlerBoard(event){
     const indexY = event.target.dataset.y;
     const indexX = event.target.dataset.x;
@@ -89,27 +101,25 @@ function gamePlay(){
     const x = +indexX;
     game.getMarkBoard(y, x);
     document.querySelector(`[data-y="${y}"][data-x="${x}"]`).textContent = board[y][x].getValue();
-    if(findWinArr(y, x, board, activePlayer()).winCondition > 0){
-      playerOneScore.textContent = playerOneCounter.getValue();
-      playerTwoScore.textContent = playerTwoCounter.getValue();
-      message.textContent = 'SCORE';
-      round.textContent = `${activePlayer().name} won the round!`;
-      findWinArr(y, x, board, activePlayer()).winArr.forEach((row, indexY) => {
-        if(row.length !== 0){
-          row.forEach(cell => boardDiv.querySelector(`[data-y="${indexY}"][data-x="${cell}"]`).classList.add('color'));
-        };
-      });
+    if(game.getWinArr(y, x)[y].length > 0 || gamePlayers.runCounter().getValue() === 9){
+      if(gamePlayers.runCounter().getValue() === 9){
+        round.textContent = 'Round DROW';
+      }else{
+        game.getWinArr(y, x).forEach((row, indexY) => {
+          if(row.length !== 0){
+            row.forEach(cell => boardDiv.querySelector(`[data-y="${indexY}"][data-x="${cell}"]`).classList.add('color'));
+          };
+        });
+        round.textContent = `${activePlayer().name} won the round!`;
+      };
       nextRound.style.visibility = 'visible';
+      playerOneScore.textContent = gamePlayers.getPlayers()[0].counter.getValue();
+      playerTwoScore.textContent = gamePlayers.getPlayers()[1].counter.getValue();
+      message.textContent = 'SCORE';
       while(board.length > 0) board.shift();
       boardDiv.removeEventListener("click", clickHandlerBoard);
-    }else if(runCounter.getValue() < 9){
+    }else if(gamePlayers.runCounter().getValue() < 8){
       message.innerHTML = `Next mark<br><span class="mark">${activePlayer().mark}</span>`;
-    }else{
-      message.textContent = 'SCORE';
-      round.textContent = 'Round DROW';
-      nextRound.style.visibility = 'visible';
-      while(board.length > 0) board.shift();
-      boardDiv.removeEventListener("click", clickHandlerBoard);
     };
     if(gameCounter.getValue() === +maxCount.value){
       nextRound.style.visibility = 'hidden';
@@ -117,41 +127,32 @@ function gamePlay(){
       setTimeout(() => {
         gameInfo.style.display = 'flex';
         const para = gameInfo.querySelector('p');
-        (playerOneCounter.getValue() > playerTwoCounter.getValue()) ?
+        (gamePlayers.getPlayers()[0].counter.getValue() > gamePlayers.getPlayers()[1].counter.getValue()) ?
           para.innerHTML = `GAME OVER,<br>WINNER<br>${playerOneName.textContent}`:
           para.innerHTML = `GAME OVER,<br>WINNER<br>${playerTwoName.textContent}`;
-        if(playerOneCounter.getValue() === playerTwoCounter.getValue())
+        if(gamePlayers.getPlayers()[0].counter.getValue() === gamePlayers.getPlayers()[1].counter.getValue())
           para.innerHTML = 'GAME OVER<br>IN A DROW';
         while(boardDiv.firstChild) boardDiv.removeChild(boardDiv.firstChild);
-        while(board.length > 0) board.shift();
       }, 1000);
     };
   }
-  boardDiv.addEventListener("click", clickHandlerBoard);
 }
 
 function playController(){
   const board = createBoard().board;
-  const players = [
-    {name: playerOneName.textContent, mark: '🗙', counter: playerOneCounter},
-    {name: playerTwoName.textContent, mark: '🞇', counter: playerTwoCounter}
-  ];
-  const activePlayer = () => players[runCounter.getValue()%2];
+  const getWinArr = (y, x) => findWinArr(y, x, board, gamePlayers.activePlayer()).winArr;
   const getMarkBoard = (y, x) => {
-    if(board[y][x].getValue() !== ''){
-      return;
-    }else{
-      board[y][x].setValue(activePlayer().mark);
-      if(findWinArr(y, x, board, activePlayer()).winCondition > 0) {
-        activePlayer().counter.increment();
-        gameCounter.increment();
-      }else{
-        runCounter.increment();
-        if(runCounter.getValue() === 9) gameCounter.increment();
-      };
+    if(board[y][x].getValue() !== ''){ return };
+    board[y][x].setValue(gamePlayers.activePlayer().mark);
+    if(getWinArr(y, x)[y].length > 0){
+      gamePlayers.activePlayer().counter.increment();
+      gameCounter.increment();
+    }else if(gamePlayers.runCounter().getValue() < 9){
+      gamePlayers.runCounter().increment();
+      if(gamePlayers.runCounter().getValue() === 9) gameCounter.increment();
     };
   };
-  return{ getMarkBoard, activePlayer, board, players };
+  return{ getMarkBoard, board, getWinArr };
 }
 
 function makeCounter(){
@@ -165,17 +166,16 @@ function makeCounter(){
 };
 
 function findWinArr(y, x, gameBoard, player){
-  let winCondition = 0;
   const winArr = [[],[],[]];
     gameBoard.forEach((row, indexY, arr) => {
-      if(arr[indexY-1] && arr[indexY+1] && arr[indexY-1][x].getValue()===player.mark &&
-      row[x].getValue()===player.mark && arr[indexY+1][x].getValue()===player.mark){
-        winArr[indexY-1].push(x); winArr[indexY].push(x); winArr[indexY+1].push(x);
+      if(arr[indexY+2] && arr[indexY+2][x].getValue()===player.mark &&
+      arr[indexY+1][x].getValue()===player.mark && arr[indexY][x].getValue()===player.mark){
+        winArr[indexY].push(x); winArr[indexY+1].push(x); winArr[indexY+2].push(x);
       }else{
         row.forEach((cell, index, arr)=>{
-          if(arr[index-1] && arr[index+1] && arr[index-1].getValue()===player.mark &&
-          cell.getValue()===player.mark && arr[index+1].getValue()===player.mark){
-            winArr[y].push(index-1, index, index+1);
+          if(arr[index+2] && arr[index+2].getValue()===player.mark &&
+          arr[index+1].getValue()===player.mark && arr[index].getValue()===player.mark){
+            winArr[y].push(index, index+1, index+2);
           }else if(gameBoard[indexY+2] && gameBoard[indexY+2][index+2] && gameBoard[indexY+2][index+2].getValue()===player.mark &&
           gameBoard[indexY+1][index+1].getValue()===player.mark && cell.getValue()===player.mark){
             winArr[indexY+2].push(index+2); winArr[indexY+1].push(index+1); winArr[indexY].push(index);
@@ -186,8 +186,7 @@ function findWinArr(y, x, gameBoard, player){
         });
       };
     });
-  if(winArr[y].length > 0) winCondition += 1;
-  return { winArr, winCondition };
+  return { winArr };
 }
 
 function Cell(){
